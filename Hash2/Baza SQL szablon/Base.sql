36c07637afd0b9f199d1d3e1bd955248fb2ae0d2
piotrek490 <44521611+piotrek490@users.noreply.github.com>
2019-06-21 12:34:50 +0200

    Add files via upload

--- Hash2/Baza SQL szablon/Base.sql
+++ Hash2/Baza SQL szablon/Base.sql
@@ -5,8 +5,8 @@ create database HaszSet DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 use HaszSet;
 
 create table MainTableData(
-val int(20),
-valNext int(20),
+val bigint,
+valNext bigint,
 dataKey text,
 isFirst int(1)
 )CHARSET=utf8;
